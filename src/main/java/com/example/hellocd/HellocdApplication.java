5f51a4e42b7b7a8e2d109ce9da2796a4818c272d
Bryan Eriksson <bryaneriksson3@gmail.com>
2024-02-26 05:06:32 +0000

    added missing annotations

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -7,15 +7,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class HellocdApplication {
 
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
 
-    public static void main(String[] args) {
-        SpringApplication.run(HellocdApplication.class, args);
-    }
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellocdApplication.class, args);
+	}
 
 }
+
